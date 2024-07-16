93a73769f13c53e5fcc2562789e8fdeba43de71d
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2024-07-16 00:31:40 -0300

    Add files via upload

--- oppenheim_2_6.m
+++ oppenheim_2_6.m
@@ -2,7 +2,7 @@
 u = @(n) double(n >= 0);
 
 % Definir os sinais
-n = -10:10; % Intervalo para n
+n = -5:5; % Intervalo para n
 
 % x[n] = ((1/3)^-n) * u[-n-1]
 x = ((1/3).^-n) .* u(-n-1);
@@ -15,21 +15,21 @@ figure;
 
 x_1 = ((1/3).^-n);
 subplot(3, 1, 1);
-stem(-10:10, x_1);
+stem(-5:5, x_1);
 title('x[n] = (1/3)^{-n}');
 xlabel('n');
 ylabel('x[n]');
 
 x_2 = (u(-n));
 subplot(3, 1, 2);
-stem(-10:10, x_2);
+stem(-5:5, x_2);
 title('x[n] = u(-n)');
 xlabel('n');
 ylabel('x[n]');
 
 x_3 = (u(-n -1));
 subplot(3, 1, 3);
-stem(-10:10, x_3);
+stem(-5:5, x_3);
 title('x[n] = u(-n-1)');
 xlabel('n');
 ylabel('x[n]');
@@ -46,26 +46,26 @@ y_conv = zeros(1, len_conv);
 
 % Calcular a convolução manualmente
 for n = 1:len_conv
-    for m = 1:len_x
-        if (n - m + 1 > 0) && (n - m + 1 <= len_h)
-            y_conv(n) = y_conv(n) + x(m) * h(n - m + 1);
+    for k = 1:len_x
+        if (n - k + 1 > 0) && (n - k + 1 <= len_h)
+            y_conv(n) = y_conv(n) + x(k) * h(n - k + 1);
         end
     end
 end
 
 % Definir o novo intervalo para a sequência convoluída
-n_conv = (-10) + (0:len_conv-1);
+n_conv = (-5) + (0:len_conv-1);
 
 % Plotar os sinais e o resultado da convolução
 figure;
 subplot(3, 1, 1);
-stem(-10:10, x);
+stem(-5:5, x);
 title('x[n] = ((1/3)^{-n}) * u[-n-1]');
 xlabel('n');
 ylabel('x[n]');
 
 subplot(3, 1, 2);
-stem(-10:10, h);
+stem(-5:5, h);
 title('h[n] = u[n-1]');
 xlabel('n');
 ylabel('h[n]');
